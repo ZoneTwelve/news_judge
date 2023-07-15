dd15f332eeb90a96b521fc6770370e0f684207a8
ZoneTwelve <zonetwelve159@gmail.com>
2023-07-15 13:31:04 +0800

    [update] automatic parse all the content, but openai api is not available

--- prompt.py
+++ prompt.py
@@ -13,6 +13,12 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 # Parse the argements
 parser = argparse.ArgumentParser(description='OpenAI News Inferencer', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--config', type=str, default=None, help='The configration you want (ex. --config prompts/config.json)')
+# Temperoary config
+parser.add_argument('--target', type=str, default=None, help="target")
+parser.add_argument('--news-title', type=str, default=None, help="news title")
+parser.add_argument('--news-content', type=str, default=None, help="news content")
+parser.add_argument('--crime-keywords', type=str, default=None, help="target")
+parser.add_argument('--judge-keywords', type=str, default=None, help="target")
 args = parser.parse_args()
 
 dirname = os.path.dirname(args.config)
@@ -28,7 +34,21 @@ prompt_file = os.path.join(dirname, prompt_config['file'])
 with open(prompt_file, encoding='utf-8') as file:
   prompt = file.read()
 print(prompt)
-exit()
+
+
+def prompt_conversion(prompt, keys, inputs):
+  print(inputs)
+  final_prompt = prompt
+  for key in keys:
+    print('READ', inputs[key])
+    with open(inputs[key]) as file:
+      c = file.read() # content
+    final_prompt = final_prompt.replace(f'${key}', c)
+  print('--- final prompt ---')
+  print(final_prompt)
+  print('-'*20)
+prompt_conversion( prompt=prompt, keys=prompt_config['inputs'], inputs=vars(args) )
+# exit()
 # Define the temperature for controlling the randomness of the output
 temperature = 0.0
 
